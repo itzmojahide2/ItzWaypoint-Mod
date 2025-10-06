e809a723ee6461c4b2e90a74fd1afa46f1e3a5f8
itzmojahide2 <itzmojahide@gmail.com>
2025-10-06 17:07:15 +0600

    Create Waypoint.java

---
+++ src/main/java/com/itzmojahide/itzwaypoint/waypoint/Waypoint.java
@@ -0,0 +1,17 @@
+package com.itzmojahide.itzwaypoint.waypoint;
+
+public class Waypoint {
+    public String name;
+    public double x, y, z;
+    public int color;
+
+    public Waypoint() {}
+
+    public Waypoint(String name, double x, double y, double z, int color) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.color = color;
+    }
+}
